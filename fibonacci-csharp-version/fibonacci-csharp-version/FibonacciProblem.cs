532f732b7ffa320fc9b984020a53385ce3df8c09
josephbulger <joseph@josephbulger.com>
2011-08-30 14:43:09 -0400

    adding fibonacci csharp version

---
+++ fibonacci-csharp-version/fibonacci-csharp-version/FibonacciProblem.cs
@@ -0,0 +1,25 @@
+using ficonacci_printer;
+
+namespace fibonacci_csharp_version
+{
+    public class FibonacciProblem
+    {
+        //use TestConsole.Print(message) to print the output of one item
+        //example: TestConsole.Print(1);
+
+        /*
+        Write a program that prints the fibonacci sequence with seed values from 0 to 15. 
+
+        The fibonacci sequence can be represented mathematically as the following:
+
+        f(n) = f(n-1) + f(n-2)
+
+        f(0) = 0
+        f(1) = 1
+        */
+        public static void Fibonacci()
+        {
+
+        }
+    }
+}
