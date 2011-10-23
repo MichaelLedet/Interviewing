a59833f72ae6f121d747b565aa4a8f1ecd28a444
Joseph Bulger <joseph@josephbulger.com>
2011-10-22 22:44:09 -0400

    adding string calculator kata as interview question

---
+++ dotnet-questions/c-sharp-version/StringCalculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_version.StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet-questions/c-sharp-version/StringCalculator/Problem.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace c_sharp_version.StringCalculator
+{
+    [TestFixture]
+    public class Problem
+    {
+        [Test]
+        public void Add_WhenGivenNoNumbers_ReturnsZero()
+        {
+             
+        }
+
+        [Test]
+        public void Add_WhenGivenOneNumber_ReturnsThatNumber()
+        {
+            
+        }
+
+        [Test]
+        public void Add_WhenGivenTwoNumbers_AddNumbersTogether()
+        {
+            
+        }
+
+        [Test]
+        public void Add_WhenGivenNewLinesBetweenNumbers_TreatsNewLineAsDelimiter()
+        {
+            
+        }
+
+        [Test]
+        public void Add_WhenGivenTwoSlashesAtBeginning_UsesDelimiterAfterSlashesInsteadOfComma()
+        {
+            
+        }
+
+        [Test]
+        public void Add_WhenGivenNegativeNumbers_ThrowsExceptionSayingNegativesArentAllowed()
+        {
+
+        }
+    }
+}
